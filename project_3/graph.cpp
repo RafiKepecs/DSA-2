87755de7747b532fe507c4d21c248d5b04d6dcbb
RafiKepecs <rafikepecs@gmail.com>
2020-11-02 00:05:19 -0500

    it's close... not sure what's wrong with dij

--- project_3/graph.cpp
+++ project_3/graph.cpp
@@ -25,6 +25,7 @@ int Graph::insert(string vs, string ve, int cost){
   if(mapping.contains(vs)){
     Edge* e = new Edge;
     e->dest = ve;
+    e->cost = cost;
     Vertex *pn = static_cast<Vertex *> (mapping.getPointer(vs));
     pn->adj_list.push_back(e);
     // cout << pn->vert << endl;
@@ -46,6 +47,8 @@ int Graph::dijkstra(string vs){
   s->known = true;
   s->vert = vs;
 
+  cout << s->vert << ": " << s->dist << " [" << s->path->vert << "]" << endl;
+
   /*
   initiale the HEAP
   set all the vertices to have infinite length and known bool to false
@@ -64,29 +67,65 @@ int Graph::dijkstra(string vs){
     w = static_cast<Vertex *> (mapping.getPointer(it->dest));
     w->dist = it->cost;
     w->vert = it->dest;
+    w->path = s;
     h.insert(it->dest, it->cost);
+    // cout << it->cost << endl;
   }
   string pId = "";
   int shortest_path = 0;
   int net_dist = 0;
   while(!h.deleteMin(&pId, &shortest_path)){
+    // cout << "0.2" << endl;
+    // s->vert = v->vert;
+    // s->path = v->path;
+    // cout << "0.3" << endl;
     v = static_cast<Vertex *> (mapping.getPointer(pId));
     v->dist = shortest_path;
+    // cout << pId << endl;
     v->vert = pId;
     v->known = true;
+    // cout << "0.4" << endl;
+    // v->path = s;
+    // cout << "0.5" << endl;
+    cout << v->vert << ": " << v->dist << " [" << vs << ", ";
+    Vertex* test = v;
+    // cout << "0.6" << endl;
+    while(test->path->vert != vs){
+      cout << test->path->vert << ", ";
+      test = test->path;
+    }
+    cout << "]" << endl;
     for(auto it : v->adj_list){
+      // if(!h.mapping.contains(it->dest)){
+      //   h.insert(it->dest, (v->dist + it->cost));
+      // }
+
+      w = static_cast<Vertex *> (mapping.getPointer(it->dest));
+      // w->dist = it->cost;
+      // w->vert = it->dest;
+
+      // if(h.insert(it->dest, it->cost) == 2){
+      //
+      //   if()
+      // }
+      // cout << it->cost << ", " << w->dist << endl;
+
       if((v->dist + it->cost) < w->dist){
         w->dist = v->dist + it->cost;
+        // cout << it->cost << endl;
         w->path = v;
         w->vert = it->dest;
         h.insert(w->vert, w->dist);
       }
-      // cout << w->vert << endl;
+      // cout << "0.1" << endl;
     }
   }
-  for(auto verts : vertices){
-    cout << verts->path->vert << endl;
-  }
+  cout << "print" << endl;
+  // for(auto verts : vertices){
+  //   for(auto it : verts->path){
+  //     cout << it->vert << endl;
+  //   }
+  // }
   return 0;
 }
 
@@ -94,7 +133,7 @@ void Graph::display(){
   for(auto verts : vertices){
     cout << verts->vert << ": " << "[";
     for(auto edges : verts->adj_list){
-      cout << edges->dest;
+      cout << edges->dest << ": " << edges->cost;
       if(edges != *verts->adj_list.end()){
         cout << ", ";
       }
